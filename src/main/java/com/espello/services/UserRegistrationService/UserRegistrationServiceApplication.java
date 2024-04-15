bb207a5912c0c1080375af2822bd07f8330914fe
Navneet Agarwal <navneet.agarwal@shiksha.com>
2024-04-16 00:12:46 +0530

    changes

--- src/main/java/com/espello/services/UserRegistrationService/UserRegistrationServiceApplication.java
+++ src/main/java/com/espello/services/UserRegistrationService/UserRegistrationServiceApplication.java
@@ -3,6 +3,10 @@ package com.espello.services.UserRegistrationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @ComponentScan({"com.espello"})
 @SpringBootApplication
@@ -11,5 +15,21 @@ public class UserRegistrationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserRegistrationServiceApplication.class, args);
 	}
+	
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // Optional: only if you need credentials
+        config.addAllowedOrigin("http://192.168.1.3:3000");
+        config.addAllowedOrigin("http://192.168.1.3:3001");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config); // Apply CORS configuration to all paths
+        return new CorsFilter(source);
+    }
 	
 }
