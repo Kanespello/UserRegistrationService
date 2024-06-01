fc554736a1f67188909a1a010e846e255a25a151
Navneet Agarwal <navneet.agarwal@shiksha.com>
2024-06-01 17:23:28 +0530

    SS-0: changes

--- src/main/java/com/espello/services/UserRegistrationService/Repository/AssistantThreadRepository.java
+++ src/main/java/com/espello/services/UserRegistrationService/Repository/AssistantThreadRepository.java
@@ -1,10 +1,11 @@
 package com.espello.services.UserRegistrationService.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 import com.espello.services.UserRegistrationService.Domain.AssistantThreads;
 
 public interface AssistantThreadRepository extends JpaRepository<AssistantThreads, Integer> {
 
+	@Query(value = "SELECT * FROM AssistantThreads WHERE sessionId = :sessionId ORDER BY submit_date DESC LIMIT 1", nativeQuery = true)
 	AssistantThreads findBySessionId(String sessionId);
 }
